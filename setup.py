97ceb3f828509ed2a16605220a19d79b4aea5c29
Parth Patel <129002378+patelandpatel@users.noreply.github.com>
2025-10-20 21:16:06 -0500

    Change author name in setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     packages=find_packages(),
     version='0.1.0',
     description='An AI solution to detect whether the domain is real or malicious',
-    author='Saurabh Naik',
+    author='Parth Patel',
     license='MIT',
 )
